8038c7f393e5dd6d9ee77b5f7878a4c0ba692a05
bboots-AU <a3057821@student.adelaide.edu.au>
2026-03-25 14:57:00 +0930

    WIP: saving progress before switching branches

--- src/viewData/student/StudentEssaySubmissionViewData.ts
+++ src/viewData/student/StudentEssaySubmissionViewData.ts
@@ -23,7 +23,7 @@ export interface EssaySubmission {
 export type EssayUpload = {
     placeHolderText: string;
     fileName: string;
-    fileType: AcceptedFileTypes;
+    fileType: AcceaptedFileTypes;
     filePath:string;
     fileProvided: boolean;
     isValid: boolean;
@@ -31,5 +31,5 @@ export type EssayUpload = {
 };
 
 // Union type defines acceptable file format for essay upload
-export type AcceptedFileTypes = "txt" | "pdf" | "docx";
+export type AcceaptedFileTypes = "txt" | "pdf" | "docx";
 
