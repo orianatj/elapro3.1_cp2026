46a3e5b997baa8f0114e5ad896f7dbd668b53b14
Oriana Tait-Jones <taioy001@mymail.unisa.edu.au>
2026-04-01 10:51:01 +1100

    Re-add type to generic type T in type parameters.Overriden in merge conflict.

--- src/viewData/student/common/StudentFilter.ts
+++ src/viewData/student/common/StudentFilter.ts
@@ -1,5 +1,5 @@
 // Type defines reusable filter DTO for student dashboard pages
-export type StudentFilter<T> = {
+export type StudentFilter<T extends string> = {
     title: string;  // ex. "Choose an IELTS Type"
     selected: T;  // ex. "task-one"
     options: FilterOption<T>[];
@@ -11,23 +11,12 @@ export type TaskType = "task-one" | "task-two";
 export type ViewBy = "weekly" | "monthly" | "quarterly";
 
 // Define union type to account for UI selection option 'All'
-export type FilterValue<T> = T | "all";
+export type FilterValue<T extends string> = T | "all";
 
 // Type defines a filter option
-export type FilterOption<T> = {
+export type FilterOption<T extends string> = {
     value: T;  // ex. "academic" or "task-one"
     label: string;  // ex. "Academic" or "Task 1"
 };
 
-<<<<<<< HEAD
-=======
-// Type defines reusable filter DTO for student dashboard pages
-export type StudentFilter<T> = {
-    title: string;  // ex. "Choose an IELTS Type"
-    selected?: T;  // ex. "task-one"
-    options: FilterOption<T>[];
-};
-
-
->>>>>>> 7ad6a8150404c9e7eb6c79936ddea7b274951b37
 
