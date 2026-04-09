2e9c6d698b6d144b85dfb26a95af684e6a3d28ed
Oriana Tait-Jones <taioy001@mymail.unisa.edu.au>
2026-04-09 12:01:38 +0930

    Define component hierarchy skeleton using top-down approach.

---
+++ src/components/student/StudentDashboardPage.tsx
@@ -0,0 +1,52 @@
+// Build a static version of the student dashboard page using React components 
+// Use top-down approach: start by building the components at the top of the hierarchy first 
+
+
+export default function StudentDashboard(){
+    return(
+    <></>;
+    <></>;
+
+    ); 
+   
+}
+
+function GreetingBanner({}){
+    return(
+        <div></div>
+    );
+};
+
+function StatsSummary({}: StatCard){
+    return();
+
+}
+
+function StatCard({}){
+    return();
+}
+
+function ProgressTracking({}){
+    return();
+}
+
+function FilterBar({}:ChartFilter){
+    return();
+}
+
+function ChartFilter({}){
+    return();
+}
+
+function CriterionSelector({}:CriterionToggle){
+    return();
+}
+
+function CriterionToggle({}){
+    return();
+}
+
+function ProgressTrackingChart({}){
+    return();
+}
+
