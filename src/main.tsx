c580bed5f58b92b37fd303aa64f7470f53c9e161
Alyssa-UNIADE <alyssa.horvath@student.adelaide.edu.au>
2026-04-16 10:23:27 +1000

    Phone update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './pages/teacher/teacher.css'
+import App from './pages/teacher/teacher.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
