7efcb3bd0dd54d7f893b199249e37cb17dc4b4ac
Alyssa-UNIADE <alyssa.horvath@student.adelaide.edu.au>
2026-04-29 13:45:20 +1000

    fixed deletion of elements

--- src/pages/teacher/teacher.tsx
+++ src/pages/teacher/teacher.tsx
@@ -1,7 +1,57 @@
 import React from "react";
 import "./teacher.css";
-import Stats from "../../types/teacher/StatisticBoxTemplate.tsx";
-import Sidebar from "../../common/SideBarTeacher.tsx";
+import Stats from "../../types/teacher/StatisticBoxTemplate";
+import Sidebar from "./../../common/SideBarTeacher.tsx";
+import MenuList from "./../../common/MenuList.tsx";
+import type { MenuData } from "./../../common/MenuList.tsx";
+
+const recentAssignments: MenuData[] = [
+  {
+    id: "1",
+    day: "23",
+    title: "Learning Basics 2a",
+    time: "12 P.M.",
+    status: "Upcoming",
+  },
+  {
+    id: "2",
+    day: "24",
+    title: "Biology Assessment 1b",
+    time: "2 P.M.",
+    status: "Upcoming",
+  },
+  {
+    id: "3",
+    day: "25",
+    title: "Analysis Comp 2c",
+    time: "4 P.M.",
+    status: "Upcoming",
+  },
+];
+
+const submissionView: MenuData[] = [
+  {
+    id: "4",
+    title: "Steven Stone",
+    status: "On Time",
+    avatarSrc: "src/assets/ClipboardList.png",
+    isAvatar: true,
+  },
+  {
+    id: "5",
+    title: "Joyle Jackie",
+    status: "On Time",
+    avatarSrc: "src/assets/ClipboardList.png",
+    isAvatar: true,
+  },
+  {
+    id: "6",
+    title: "Kyle Cone",
+    status: "LATE",
+    avatarSrc: "src/assets/ClipboardList.png",
+    isAvatar: true,
+  },
+];
 
 export default function Dashboard() {
   return (
