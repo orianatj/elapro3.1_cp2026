f8d299e8e38c79119f4538f5bcf56e356b7e02b3
bboots-AU <a3057821@student.adelaide.edu.au>
2026-05-02 08:11:46 +0930

    refactor(AD-333): move Submission.tsx to the pages folder and remove placeholder Reports file

--- src/pages/student/Reports.tsx
+++
@@ -1,8 +0,0 @@
-export default function ReportsPage() {
-    return (
-
-        <>
-            <h1>This is the Reports Page</h1>
-        </>
-    )
-};
--- src/pages/student/Submissions.tsx
+++ src/pages/student/Submissions.tsx
@@ -1,15 +1,15 @@
 
 // Import the page-level ViewData contract for the Student Submissions page,
 // to define the complete UI-ready data structure expected by this component.
-import type { StudentSubmissions } from "../types/student/StudentSubmissionsViewData";
+import type { StudentSubmissions } from "../../types/student/StudentSubmissionsViewData";
 
 // Import the shared PageHeaderView component
-import { PageHeaderView } from "../common/PageHeaderView";
+import { PageHeaderView } from "../../common/PageHeaderView";
 
 // Import page-specific child components,
 // responsible for rendering distinct sections of the page.
-import { SubmissionsFilters } from "./SubmissionsFilters";
-import { SubmissionsTable } from "./SubmissionsTable";
+import { SubmissionsFilters } from "../../studentDashboard/SubmissionsFilters";
+import { SubmissionsTable } from "../../studentDashboard/SubmissionsTable";
 
 // Define the Props type for the page-level component.
 type Props = {
@@ -17,7 +17,7 @@ type Props = {
 }
 
 // Page entry component for the Student Submissions screen.
-export function StudentSubmissionsPage({viewData}: Props) {
+export function SubmissionsPage({viewData}: Props) {
     return (
         <div className = "student-submissions-page">
             <PageHeaderView header = {viewData.pageHeader} />
