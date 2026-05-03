1b36a3b3bd86009ed250736d84b5f6229c35b1a1
Alyssa-UNIADE <alyssa.horvath@student.adelaide.edu.au>
2026-05-04 09:40:47 +1000

    Fixed App Router

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminLayout from "../layouts/AdminLayout";
 import StudentLayout from "../layouts/StudentLayout";
-import TeacherLayout from "../layouts/TeacherLayout";
-import EditStudentScorePage from "../pages/teacher/EditStudentScore.tsx";
 import { AdminDashboardPage } from "../pages/admin/adminDashboard.tsx";
-//import TeacherLayout from "../layouts/TeacherLayout";
+import TeacherLayout from "../layouts/TeacherLayout";
+import TeacherDashboard from "../pages/teacher/teacher.tsx";
+import EditStudentScorePage from "../pages/teacher/EditStudentScore.tsx";
 //import AuthLayout from "../layouts/AuthLayout";
 import EssaySubmissionPage from "../pages/student/EssaySubmission";
 import StudentDashboardPage from "../pages/student/StudentDashboard";
 import PracticeWritingPage from "../pages/student/PracticeWriting";
 import SubmissionAnalysisPage from "../pages/student/SubmissionAnalysis";
 import { SubmissionsPage } from "../pages/student/Submissions";
-//import TeacherDashboard from "../pages/teacher/teacher.tsx";
 
 // TODO: Confirm if student are the only user type that require an account management page
 
@@ -34,11 +33,11 @@ export default function AppRouter() {
           <Route path="submissions" element={<SubmissionsPage />} />
           <Route path="submissions/:submissionId" element={<SubmissionAnalysisPage />} />
         </Route>
-        <Route path="/teacher" element={<TeacherLayout />}>
+        {
+     <Route path="/teacher" element={<TeacherLayout />}>
         <Route index element={<TeacherDashboard />} />
         <Route path="edit-score" element={<EditStudentScorePage />} />
-        {
-
+      </Route>
           /*</Route>
                   // Teacher Dashboard
                   
