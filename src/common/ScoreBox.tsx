0a18b150fefe25da00f1a318184191ebda3f0f0a
Alyssa-UNIADE <alyssa.horvath@student.adelaide.edu.au>
2026-05-05 09:14:38 +1000

    Functionality added for scoring and sliders

--- src/common/ScoreBox.tsx
+++ src/common/ScoreBox.tsx
@@ -2,18 +2,30 @@ import React from "react";
 
 type ScoreBoxProps = {
   title: string;
-  score: string;
+  score: number;
+  onScoreChange: (value: number) => void;
 };
 
-export default function ScoreBox({ title, score }: ScoreBoxProps) {
+export default function ScoreBox({
+  title,
+  score,
+  onScoreChange,
+}: ScoreBoxProps) {
   return (
     <div className="score-box">
       <h4>{title}</h4>
-      <h2>{score}</h2>
+      <h2>{score.toFixed(1)}</h2>
 
       <div className="slider-wrapper">
         <div className="score-slider">
-          <input type="range" />
+          <input
+            type="range"
+            min="1"
+            max="10"
+            step="0.5"
+            value={score}
+            onChange={(e) => onScoreChange(Number(e.target.value))}
+          />
         </div>
       </div>
     </div>
