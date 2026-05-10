ed1a4e43b412241d19f1444c56e47edb91929aed
Oriana Tait-Jones <taioy001@mymail.unisa.edu.au>
2026-05-10 13:44:51 +0930

    Update comment to improve accuracy

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,8 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
 import { register as registerApi } from "../services/authApi.ts"
 
-/* Define TanStack Query mutation hook that takes user credentials,
-calls the register API and returns a TSQ mutation object
+/* Define TanStack Query mutation hook that wraps the Axios /auth/register api
+call and returns a TSQ mutation object
 */
 export function useRegister() {
     return useMutation({
