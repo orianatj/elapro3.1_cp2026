f14db6da05723e32eebeb79790eebb64f298586f
bboots-AU <a3057821@student.adelaide.edu.au>
2026-05-10 11:33:15 +0930

    AD-379: Import Submissions page dependencies without executing them

--- src/pages/student/StudentSubmissions.tsx
+++ src/pages/student/StudentSubmissions.tsx
@@ -1,4 +1,17 @@
-export default function SubmissionsPage() {
+
+// Import the shared PageHeaderView component
+import { PageHeaderView } from "../../common/PageHeaderView";
+
+// Import page-specific child components,
+// responsible for rendering distinct sections of the page.
+import { SubmissionsFilters } from "../../studentDashboard/SubmissionsFilters";
+import { SubmissionsTable } from "../../studentDashboard/SubmissionsTable";
+
+// Import the page-level data hook responsible for fetching submissions and producing ViewData
+import { useStudentSubmissions } from "../../hooks/useStudentSubmissions";
+
+export default function SubmissionsPage() { 
+
     return (
 
         <>
