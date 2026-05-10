a22fed7a09de56ad5f9fdab1652082580f7b3da9
Oriana Tait-Jones <taioy001@mymail.unisa.edu.au>
2026-05-10 13:38:34 +0930

    Add TSQ mutation hook for user registration

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,3 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
-import { register } from "../services/authApi.ts"
+import { register as registerApi } from "../services/authApi.ts"
 
+/* Define TanStack Query mutation hook that takes user credentials,
+calls the register API and returns a TSQ mutation object
+*/
+export function useRegister() {
+    return useMutation({
+        mutationFn: registerApi,
+    });
+}
