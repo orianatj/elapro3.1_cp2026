c9f05ba12493a174548a7ade7e427bc72b0f38b9
Jeremy <jeremy.mannix@student.adelaide.edu.au>
2026-05-11 12:59:50 +1000

    added score info card

--- src/pages/teacher/IndividualSubmission.tsx
+++ src/pages/teacher/IndividualSubmission.tsx
@@ -8,12 +8,25 @@ export default function IndividualSubmission() {
     <>
       <div className="header">Student's Paper</div>
       <div className="flex-container">
-        <div>
-          <h2>Task 2</h2>
-          <p>Vestibulum ac dictum risus. Praesent eget aliquam ligula. Mauris nisi  eros, dignissim ut velit vitae, facilisis luctus nisl. Morbi lacinia ex  eu tortor condimentum tempus. Quisque sagittis blandit leo, in rhoncus  erat rhoncus vitae. Vivamus purus turpis, consequat vel arcu vitae,  rutrum congue massa. Suspendisse id ipsum nec nunc lobortis viverra ac ultricies sapien. Maecenas at mauris non quam lobortis molestie. Vivamus euismod urna ac nisi posuere, in condimentum tellus posuere. Proin id  vehicula risus, dapibus pellentesque metus. Quisque eleifend auctor diam vel facilisis. Nulla facilisi. In venenatis ligula sit amet odio  pulvinar, id feugiat enim pellentesque. In faucibus tempus nulla, nec  aliquet libero tincidunt sit amet. </p>
+        <div className ="submission-view-card">
+          <div>
+            <h2>Task 2</h2>
+            <p>Vestibulum ac dictum risus. Praesent eget aliquam ligula. Mauris nisi  eros, dignissim ut velit vitae, facilisis luctus nisl. Morbi lacinia ex  eu tortor condimentum tempus. Quisque sagittis blandit leo, in rhoncus  erat rhoncus vitae. Vivamus purus turpis, consequat vel arcu vitae,  rutrum congue massa. Suspendisse id ipsum nec nunc lobortis viverra ac ultricies sapien. Maecenas at mauris non quam lobortis molestie. Vivamus euismod urna ac nisi posuere, in condimentum tellus posuere. Proin id  vehicula risus, dapibus pellentesque metus. Quisque eleifend auctor diam vel facilisis. Nulla facilisi. In venenatis ligula sit amet odio  pulvinar, id feugiat enim pellentesque. In faucibus tempus nulla, nec  aliquet libero tincidunt sit amet. </p>
+          </div>
+          <div>
+            Word Count: 1200  Errors Found: 5   Suggestions: 4
+          </div>
         </div>
+        
         <div>
-          <p>Total Score</p>
+          <div className="total-score-info">
+            <h2>Total Score</h2>
+            <p>Task Response: 6.0 - Addresses the topic but lacks depth in argumentation.</p>
+            <p>Coherence & Cohesion: 6.0 - Well structured with clear paragraphs</p>
+            <p>Lexical Resource: 6.5 - Limited range of vocabulary, some errors.</p>
+            <p>Grammatical Range & Accuracy 6.0 - Several grammar mistakes and sentence errors</p>
+
+          </div>
 
           <div className="flex-container">
             <ToolbarButton
