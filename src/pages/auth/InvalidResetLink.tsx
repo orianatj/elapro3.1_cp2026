4141e44a03e31eaa3d35e382e6e3f6f75399faab
Oriana Tait-Jones <taioy001@mymail.unisa.edu.au>
2026-05-15 19:05:56 +0930

    Update import statement and src reference to align with icon rename

--- src/pages/auth/InvalidResetLink.tsx
+++ src/pages/auth/InvalidResetLink.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import passwordfailure from "../../assets/auth/passwordfailure.png";
+import failure from "../../assets/auth/failure.png";
 
 
 export function InvalidResetLink() {
@@ -9,7 +9,7 @@ export function InvalidResetLink() {
     return (
         <div className="auth-page" >
             <div className="auth-card">
-                <img className="auth-icon" src={passwordfailure} alt="Reset password failure icon" width={100} height={100} />
+                <img className="auth-icon" src={failure} alt="Reset password failure icon" width={100} height={100} />
                 <div className="auth-header">
                     <h2 className="auth-title">Password Reset Link Expired</h2>
                     <p className="auth-instruction">Your password reset link is invalid or has expired.</p>
