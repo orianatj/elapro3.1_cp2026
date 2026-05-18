b004585e76b832e5d2fcc75be7537116f090f55f
Oriana Tait-Jones <taioy001@mymail.unisa.edu.au>
2026-05-18 15:46:22 +0930

    Add SignupPage integration tests for conditional component rendering

--- src/pages/auth/SignupPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
-import { SignupForm } from "./SignupForm";
-import { vi } from "vitest";
-
-
-// Refactor based on new SignupPage structure/logic
-
