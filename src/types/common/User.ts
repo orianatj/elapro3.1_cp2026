64806e2e7383eb3f4e9612199aeb22531c590d28
Oriana Tait-Jones <taioy001@mymail.unisa.edu.au>
2026-05-20 17:34:36 +0930

    Improve User auth typing with role and account status union types

--- src/types/common/User.ts
+++ src/types/common/User.ts
@@ -1,3 +1,10 @@
+// Define a union type to strongly type user roles
+export type UserRole = "student" | "admin" | "supervisory-teacher" | "external-teacher";
+
+// Define a union type to strongly type account status
+export type AccountStatus = "active" | "pending" | "inactive" | "active" | "cancelled";
+
+
 // Define type for data associated with an authenticated user
 export type User = {
     userId: string;
@@ -6,5 +13,6 @@ export type User = {
     lastName: string;
     emailAddress: string;
     phoneNumber?: string;
-    userRole: string;
+    userRole: UserRole;
+    accountStatus: AccountStatus;
 };
