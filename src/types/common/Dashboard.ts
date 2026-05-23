0f819c942ffc4e3b2e77abad43f2e55d00cbdb35
Oriana Tait-Jones <taioy001@mymail.unisa.edu.au>
2026-05-23 20:23:39 +0930

    Add progress chart aggregation and transformation types

--- src/types/common/Dashboard.ts
+++ src/types/common/Dashboard.ts
@@ -5,6 +5,7 @@ export type IeltsType = "academic" | "general";
 // Define a union type to strongly type taskType
 export type TaskType = "task1" | "task2";
 
+
 // Define a type or Student Progress Tracking chart 
 export type StudentProgressTracking = {
     userId?: string;
@@ -13,3 +14,37 @@ export type StudentProgressTracking = {
     ieltsType?: IeltsType;
     taskType?: TaskType;
 };
+
+// Define type for ProgressChartPoint 
+export type ProgressChartPoint = {
+    submissionTimestamp: string;
+    taskResponse: number;
+    coherenceCohesion: number;
+    lexicalResource: number;
+    grammticalRangeAccuracy: number;
+    overallScore: number;
+};
+
+// Define a union type to strongly type chart aggregation levels 
+export type AggregationLevel = "daily" | "weekly" | "monthly" | "quarterly";
+
+
+// Define type for single chart point 
+export type AggregatedChartPoint = {
+    dateLabel: string;
+    totalSubmissions: number;
+    meanOverallScore: number;
+    meanTaskResponse: number;
+    meanCoherenceCohesion: number;
+    meanLexicalResources: number;
+    meanGrammaticalRangeAccuracy: number;
+};
+
+// Define type for data returned by useStudentProgressTracking hook
+export type ProgressChartData = {
+    shouldRender: boolean;
+    message?: string;
+    aggregationLevel?: AggregationLevel;
+    chartData?: AggregatedChartPoint[];
+};
+
