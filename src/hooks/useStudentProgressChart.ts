a6d5ab4adf564c57f757ea71e23aad73bd0cac91
Oriana Tait-Jones <taioy001@mymail.unisa.edu.au>
2026-05-25 13:08:45 +0930

    Rename hook to differentiate between progresschart and statscard hooks

---
+++ src/hooks/useStudentProgressChart.ts
@@ -0,0 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import { studentProgressTracking as StudentProgressTrackingApi } from "../services/dashboardApi";
+import type { StudentProgressTracking } from "../types/common/Dashboard";
+import { shouldRenderProgressChart, determineAggregationLevel, aggregateProgressSeries } from "../utils/progressChartAggregation";
+
+
+/* Define TanStack Query, Query hook to return progress chart data for the user's dashboard. 
+TODO: Subscribe uwith query key to new submission marked notification endpoint using InvalidateQueries functionality inside that mutation
+*/
+export function useStudentProgressChart(params: StudentProgressTracking) {
+    return useQuery({
+        queryKey: ["progresschart", params],
+        queryFn: () => StudentProgressTrackingApi(params),
+        select: (response) => {
+
+            const series = response.data.series;
+
+            const shouldRender = shouldRenderProgressChart(series);
+
+            if (!shouldRender) {
+
+                return {
+                    shouldRender,
+                    message: "Complete more practice submissions to view progress trends.",
+                };
+            }
+
+            const aggregationLevel =
+                determineAggregationLevel(series);
+
+            const chartData =
+                aggregateProgressSeries({ series, aggregationLevel });
+
+            return {
+                shouldRender,
+                aggregationLevel,
+                chartData
+            };
+        }
+    });
+}
