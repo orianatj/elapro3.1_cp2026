ee559375569d5807986ab4a1da40e70c565ebd41
Jeremy <jeremy.mannix@student.adelaide.edu.au>
2026-05-31 13:08:47 +1000

    Testing returning user details working

--- src/pages/common/ProfileSettings.tsx
+++ src/pages/common/ProfileSettings.tsx
@@ -10,7 +10,17 @@ export function ProfileSettings() {
     return (
         <div>
             <div>This is where profile settings will go</div>
+
+            <div>{userData?.firstName || "No first name found"}</div>
+            <div>{userData?.middleName || "No middle name found"}</div>
+            <div>{userData?.lastName || "No last name found"}</div>
+            <div>{userData?.phoneNumber || "No phone number found"}</div>
             <div>{userData?.emailAddress || "No email found"}</div>
+
+
+            <div>{userData?.userId || "No userid found"}</div>
+            <div>{userData?.userRole || "No role found"}</div>
+            <div>{userData?.accountStatus || "No account status found"}</div>
         </div>
     );
 
