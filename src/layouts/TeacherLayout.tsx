f590bfcb8e5c404acaabe2585bcd91a06472d284
Jeremy <jeremy.mannix@student.adelaide.edu.au>
2026-06-04 12:55:30 +1000

    Added new navbar into student dashboard and made small css amendments

--- src/layouts/TeacherLayout.tsx
+++ src/layouts/TeacherLayout.tsx
@@ -1,17 +1,28 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../common/SideBarTeacher";
+import Navbar from "../common/Navbar";
+import "./teacherlayout.css"
+import grid from "../assets/grid.png";
+import create from "../assets/create.png";
+import documents from "../assets/documents.png";
+
+const teacherNavItems = [
+  {icon: grid, label: "Dashboard", end: true, path: "/teacher" },
+  {icon: create, label: "Assignments", path: "/teacher/create-assignment"},
+  {icon: documents, label: "Submissions", path: "/teacher/submissions"},
+]
 
 export default function TeacherLayout() {
-  return (
-    <div className="teacher-container">
-      <Sidebar />
+    return (
+        <div className="student-container">
+            {/* Navigation Menu */}
+            <Navbar pageNames={teacherNavItems} />
 
-      <div className="main">
+            <div className="student-layout-body">
+                <main className="page-content">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
 
-        <main>
-          <Outlet />
-        </main>
-      </div>
-    </div>
-  );
-}
+    )
+};
