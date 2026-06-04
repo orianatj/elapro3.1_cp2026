f18f527c36afe8e9ecb35098f046db54e792c58e
Jeremy <jeremy.mannix@student.adelaide.edu.au>
2026-06-04 13:09:03 +1000

    Updated teacher navbar icons and links

--- src/layouts/TeacherLayout.tsx
+++ src/layouts/TeacherLayout.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../common/Navbar";
 import "./teacherlayout.css"
-import grid from "../assets/grid.png";
-import create from "../assets/create.png";
-import documents from "../assets/documents.png";
+import dashboard from "../assets/studentnavicons/dashboard.png";
+import submissions from "../assets/studentnavicons/practicewriting.png";
 
 const teacherNavItems = [
-  {icon: grid, label: "Dashboard", end: true, path: "/teacher" },
-  {icon: create, label: "Assignments", path: "/teacher/create-assignment"},
-  {icon: documents, label: "Submissions", path: "/teacher/submissions"},
+  {icon: dashboard, label: "Dashboard", end: true, path: "/teacher" },
+  {icon: submissions, label: "Submissions", path: "/teacher/submissions"},
 ]
 
 export default function TeacherLayout() {
