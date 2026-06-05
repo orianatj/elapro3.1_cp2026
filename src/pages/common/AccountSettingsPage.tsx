50417500be2ea3d58d84651fde41efd0e25d5270
Oriana Tait-Jones <taioy001@mymail.unisa.edu.au>
2026-06-05 22:58:29 +0930

    Add navgation back to respective dashboards

--- src/pages/common/AccountSettingsPage.tsx
+++ src/pages/common/AccountSettingsPage.tsx
@@ -3,18 +3,42 @@ import { ProfileSettings } from "./ProfileSettings";
 import { SubscriptionBillingSettings } from "./SubscriptionBillingSettings";
 import "../../pages/auth/authpages.css";
 import "../../pages/common/accountsettings.css";
+import { useNavigate } from "react-router-dom";
 
 export default function AccountSettingsPage() {
 
     // Get logged-in user's info
     const { user } = useAuth();
 
+    const navigate = useNavigate();
+
     // Create boolean variable to support conditional rendering 
     const showBillingSubs = user?.userRole === "student";
+
+    const handleBackToDashboard = () => {
+
+        if (user?.userRole === "student") {
+            navigate("/student");
+        } else if (
+            user?.userRole === "supervisory_teacher" ||
+            user?.userRole === "external_teacher"
+        ) {
+            navigate("/teacher");
+        } else if (user?.userRole === "admin") {
+            navigate("/admin");
+        }
+    };
 
     return (
 
         <div className="style-settings-container">
+
+            <button
+                className="auth-button"
+                onClick={handleBackToDashboard}
+            >
+                Back to Dashboard
+            </button>
 
             {/* Render Profile Settings sub-component */}
             <ProfileSettings />
